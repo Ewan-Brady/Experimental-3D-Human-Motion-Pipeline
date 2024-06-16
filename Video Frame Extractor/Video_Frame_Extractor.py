cf1538d55c409fe021db80d3b568cd9b8d0e2308
Ewan Brady <ewan.brady@ucalgary.ca>
2024-06-15 21:02:04 -0600

    Now extracts images from data in a HMDB51 style file structure.

--- Video Frame Extractor/Video_Frame_Extractor.py
+++ Video Frame Extractor/Video_Frame_Extractor.py
@@ -16,6 +16,8 @@ def FrameCapture(path, video_name):
   
     # checks whether frames were extracted 
     success = 1
+    
+    error_frames_count = 0
   
     while success: 
   
@@ -23,12 +25,56 @@ def FrameCapture(path, video_name):
         # function extract frames 
         success, image = vidObj.read() 
   
-        # Saves the frames with frame-count 
-        cv2.imwrite(video_name+("_frame%d.jpg" % count), image) 
+        try:
+            # Saves the frames with frame-count 
+            cv2.imwrite(video_name+("_frame%d.jpg" % count), image) 
   
-        count += 1
+            count += 1
+        except:
+            error_frames_count += 1
+            
+    if error_frames_count > 1:
+        print(video_name + " had an unexpected number of error frames, " + str(error_frames_count))
         
-#outputDirectory = ""
-#os.chdir(outputDirectory) #Change directory to output directory, use absolute path for input.
+"""
+The below iterator is made to extract from the HMDB51 dataset's directory structre. 
+The frame extraction function however works for whatever, it just spits out its output images
+into whatever is set as the current directory for the program, and you can feed an absolute path
+into the function as input. 
+"""
 
-FrameCapture("test.mp4", "test")
+inputDirectory = "/mnt/e/ML-Training-Data/HMDB51/Dataset/Dataset Extracted" #The absolute directory where the input video dataset is stored.
+outputDirectory = "/mnt/e/ML-Training-Data/HMDB51/Dataset/Dataset Extracted Images" #The absolute directory where you want it to spit out the images.
+
+os.chdir(inputDirectory) #First, go to the input directory and get its members
+actions = os.listdir() #got its members (and in HMDB51 also corresponds to main action)
+
+os.chdir(outputDirectory) #now, create corresponding output directories for each action
+for action in actions: 
+    action_path = outputDirectory + "/" + action
+    if(not os.path.exists(action_path)):
+        os.makedirs(action_path) #Create action output directory if it does not exist
+
+
+for action in actions: #now that we have created the nessecary directories, we can extract the images into them
+    os.chdir(inputDirectory + "/" + action)
+    videos = os.listdir() #get a list of the input videos.
+    
+    
+    action_output_directory = (outputDirectory + "/" + action)
+    os.chdir(action_output_directory) #return to output directory for action
+    
+    for video in videos:
+        try:    
+            frames_directory = (action_output_directory + "/" + video)
+            os.makedirs(frames_directory) #make directory for frames
+            os.chdir(frames_directory) #go to directory
+    
+            video_directory = (inputDirectory + "/" + action + "/" + video)
+            FrameCapture(video_directory,video) #Extract frames from video into present directory
+        except:
+            print("An error occured while splitting " + video)    
+        
+    print(action + " done...")
+
+print("image extraction complete!")
